4df2f34c0a04585462d6ab8125ece5050be3f568
Brian De Wolf <git@bldewolf.com>
2011-09-29 17:24:05 -0700

    Add list functionality
    Add getopt stuff to list for getting help and putting in passwords.

--- src/list.c
+++ src/list.c
@@ -19,22 +19,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <kpass.h>
 
 #include "util.h"
 
+static void print_help() {
+	puts("kputil list - list contents of a database\n\
+Usage: kputil list <options> <files>\n\
+Available options:\n\
+    -h    displays help");
+}
+
 int list_main(int argc, char* argv[]) {
 	kpass_db *db;
 	uint8_t pw_hash[32];
 	char* dest;
+	char* pw = NULL;
 	int i;
 	int mod = 0;
+	char c;
+	kpass_retval retval;
 
-	if(argc != 2) return 1;
+	while((c = getopt(argc, argv, "hp:")) != -1) {
+		switch(c) {
+			case 'h':
+				print_help();
+				return 0;
+			case 'p':
+				pw = optarg;
+				break;
+			case '?':
+				if(optopt == 'p') {
+					fprintf(stderr, "Option p requires an argument\n");
+					return 1;
+				} else {
+					fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+					return 1;
+				}
+			default:
+				fprintf(stderr, "HOW DID I GET HERE\n");
+				return 1;
 
-	if(open_file(argv[1], &db, pw_hash, 3)) {
-		puts("Open file failed");
+		}
+	}
+
+	if(optind == argc) {
+		fprintf(stderr, "No file specified.\n");
+		return 1;
+	}
+
+	if(pw) {
+		retval = kpass_hash_pw(NULL, pw, pw_hash);
+		if(retval) {
+			fprintf(stderr, "Failed to hash password: %s\n", kpass_error_str[retval]);
+			return retval;
+		}
+
+		db = open_db(argv[optind], pw_hash);
+	} else {
+		if(open_file(argv[optind], &db, pw_hash, 3)) {
+			fprintf(stderr, "Open file failed\n");
+			return 1;
+		}
 	}
 
 	if(!db)
