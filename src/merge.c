f7d7ae1cbb15028c960dd602d622a7f0e015cf60
Brian De Wolf <git@bldewolf.com>
2011-12-01 16:45:29 -0800

    Update merge mode
    Implement a merge mode, based off of copy mode and the old kpmerge husk.

--- src/merge.c
+++ src/merge.c
@@ -19,57 +19,166 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <uuid/uuid.h>
 
 #include <kpass.h>
 
 #include "lib/yesno.h"
-
 #include "util.h"
 
+static void print_help() {
+	puts("kputil merge - merge entries of a database to another\n\
+Usage: kputil merge <options>\n\
+Available options:\n\
+    -s        source database\n\
+    -d        destination database\n\
+    -o        output database (currently too timid to clobber)\n\
+    -p <pw>   the password to unlock the databases (interactive otherwise)\n\
+              If -P is set, this password is used on the first database\n\
+    -P <UUID> the UUID from the source database to use as the password for\n\
+              the destination database\n\
+    -r        reverses the order the databases are opened and causes -P to\n\
+              read from the destination database rather than the source\n\
+    -a        non-interactively merges all updated entries\n\
+    -h        this displays the help");
+}
+
 int merge_main(int argc, char* argv[]) {
-	kpass_db *dbs[2];
-	uint8_t pw_hash[2][32];
-	char* dest;
-	int i;
+	kpass_db *ddb = NULL, *sdb = NULL, **db;
+	uint8_t spwh[32];
+	uint8_t dpwh[32];
+	uint8_t *pwh;
+	char* path;
+	char* pw = NULL;
+	char* pw_uuid = NULL;
+	int reverse = 0;
+	char *src, *dst, *out;
+	char c;
+	kpass_entry *e;
+	uuid_t uuid;
 	int mod = 0;
+	uint32_t i;
+	int allyes = 0;
 
-	int target, source;
+	while((c = getopt(argc, argv, "ad:s:hp:P:o:r")) != -1) {
+		switch(c) {
+			case 's':
+				src = optarg;
+				break;
+			case 'd':
+				dst = optarg;
+				break;
+			case 'o':
+				out = optarg;
+				break;
+			case 'p':
+				pw = optarg;
+				break;
+			case 'P':
+				pw_uuid = optarg;
+				break;
+			case 'r':
+				reverse = !reverse;
+				break;
+			case 'a':
+				allyes = 1;
+				break;
+			case 'h':
+				print_help();
+				return 0;
+			case '?':
+				if(optopt == 'p' || optopt == 's' || optopt == 'd' || optopt == 'P' || optopt == 'o') {
+//					getopt prints an error here already
+//					fprintf(stderr, "Option %c requires an argument\n", optopt);
+					return 1;
+				} else {
+					fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+					return 1;
+				}
+			default:
+				fprintf(stderr, "HOW DID I GET HERE\n");
+				return 1;
 
-	if(argc != 3) return 1;
-
-	if(open_file(argv[1], &dbs[0], NULL, pw_hash[0], 3) ||
-		open_file(argv[2], &dbs[1], NULL, pw_hash[1], 3)) {
-		puts("Open file failed somehow");
+		}
 	}
 
-	if(!dbs[0] && !dbs[1])
+	if(!src) {
+		fprintf(stderr, "No source specified.\n");
 		return 1;
+	}
+	if(!dst) {
+		fprintf(stderr, "No destination specified.\n");
+		return 1;
+	}
+	if(!out) {
+		fprintf(stderr, "No output specified.\n");
+		return 1;
+	}
 
-	source = 0;
-	target = 1;
+//	Use pointer magic to accomodate reversed order in the case of using pw_uuid
+	if(!reverse) {
+		path = src;
+		db = &sdb;
+		pwh = spwh;
+	} else {
+		path = dst;
+		db = &ddb;
+		pwh = dpwh;
+	}
 
-	puts("qsorting");
+	if(open_file(path, db, pw, pwh, 3)) {
+		fprintf(stderr, "Open file %s failed\n", path);
+		goto merge_failed;
+	}
 
-	qsort(dbs[source]->entries, dbs[source]->entries_len,
-		sizeof(*dbs[source]->entries), qsort_entry);
-		
-	puts("Searching for dups");
+//	find pw_uuid and replace pw with it
+	if(pw_uuid) {
+		if(uuid_parse(pw_uuid, uuid)) {
+			fprintf(stderr, "Failed to parse UUID %s for second database password\n", pw_uuid);
+			goto merge_failed;
+		}
+		e = find_entry_ptr_uuid(*db, uuid);
+		if(!e) {
+			fprintf(stderr, "No entry found for UUID %s in %s for second database password\n", pw_uuid, path);
+			goto merge_failed;
+		}
+		pw = e->password;
+	}
 
-	for(i = 0; i < dbs[target]->entries_len; i++) {
+//	More magic
+	if(reverse) {
+		path = src;
+		db = &sdb;
+		pwh = spwh;
+	} else {
+		path = dst;
+		db = &ddb;
+		pwh = dpwh;
+	}
+
+	if(open_file(path, db, pw, pwh, 3)) {
+		fprintf(stderr, "Open file %s failed\n", path);
+		goto merge_failed;
+	}
+
+//	Do the actual merging
+	qsort(sdb->entries, sdb->entries_len,
+		sizeof(*sdb->entries), qsort_entry);
+
+//	Search for duplicates
+	for(i = 0; i < ddb->entries_len; i++) {
 		kpass_entry **br, *source_e, *target_e, *tmp;
-		char c;
 		int source_i;
 
-		if(is_metadata(dbs[target]->entries[i]))
+		if(is_metadata(ddb->entries[i]))
 			continue;
 
-		target_e = dbs[target]->entries[i];
+		target_e = ddb->entries[i];
 
-//		puts("Searching for entry:");
-//		print_entry(dbs[target]->entries[i]);
-
-		br = bsearch(&target_e, dbs[source]->entries,
-			dbs[source]->entries_len, sizeof(*dbs[source]->entries),
+		br = bsearch(&target_e, sdb->entries,
+			sdb->entries_len, sizeof(*sdb->entries),
 			qsort_entry);
 		if(br) {
 			source_e = *br;
@@ -83,17 +192,17 @@ int merge_main(int argc, char* argv[]) {
 			print_entry_diff(target_e, source_e);
 
 			printf("Replace entry? (yes/no) ");
-			if(yesno()) {
-				mod = 1;
+			if(allyes || yesno()) {
+				mod++;
 				puts("Swapping entries...");
-				source_i = find_entry_index_ptr(dbs[source], source_e);
-				tmp = dbs[source]->entries[source_i];
-				dbs[source]->entries[source_i] = target_e;
-				dbs[target]->entries[i] = tmp;
+				source_i = find_entry_index_ptr(sdb, source_e);
+				tmp = sdb->entries[source_i];
+				sdb->entries[source_i] = target_e;
+				ddb->entries[i] = tmp;
 
-				if(find_group_index_id(dbs[target], tmp->group_id) == -1) {
+				if(find_group_index_id(ddb, tmp->group_id) == -1) {
 					puts("Entry group ID not found, setting to first listed group.");
-					fix_group(dbs[target], i);
+					fix_group(ddb, i);
 				}
 			} else {
 				puts("Skipping...");
@@ -101,24 +210,24 @@ int merge_main(int argc, char* argv[]) {
 		}
 	}
 
-	puts("Okay finished");
-/*	for(i = 0; i < dbs[target]->entries_len; i++) {
-		if(is_metadata(dbs[target]->entries[i]))
-			continue;
-		print_entry(dbs[target]->entries[i]);
-	}*/
-
 	if(mod) {
-		dest = malloc(strlen(argv[target]) + 5);
-		strcpy(dest, argv[target]);
-		strcat(dest, ".new");
-		printf("Saving modified DB to %s\n", dest);
-
-		save_db(dest, dbs[target], pw_hash[target]);
-		free(dest);
+		save_db(out, ddb, dpwh);
+		printf("%d entries were updated.\n", mod);
 	} else {
 		puts("No changes found, not bothering saving.");
 	}
 
+
 	return 0;
+
+merge_failed:
+	if(sdb) {
+		kpass_free_db(sdb);
+		free(sdb);
+	}
+	if(ddb) {
+		kpass_free_db(ddb);
+		free(ddb);
+	}
+	return 1;
 }
